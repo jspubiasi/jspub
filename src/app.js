a89e5894f0deeea07f624565836711b7a2d68f1f
Pafcal <a.pavaloi@gmail.com>
2018-11-04 13:06:47 +0200

    Made menu responsive

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
+import SmoothScroll from 'smooth-scroll';
+
+new SmoothScroll('a[href*="#"]');
+
 import "./font.css";
 import "./app.css";
 
 console.log("bubu");
+
+const headerEl = document.getElementsByTagName("header")[0];
+
+document.getElementById("menuBtn").addEventListener("click", () => {
+    headerEl.classList.toggle("header--menu-open");
+});
+
+document.querySelectorAll("header a").forEach(el => {
+    el.addEventListener("click", () => {
+        headerEl.classList.remove("header--menu-open");
+    });
+})
